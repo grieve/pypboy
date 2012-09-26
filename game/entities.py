513edea5dd3a1ca5ba2fec80faae9aa7637c1f87
Ryan Grieve <me@ryangrieve.com>
2012-09-26 14:23:11 +0100

    Added mapsquare and mapgrid entities to break down map creation into chunks

--- game/entities.py
+++ game/entities.py
@@ -192,3 +192,99 @@ class Map(game.Entity):
 			except Exception, e:
 				print(e)
 				pass
+
+
+class MapSquare(game.Entity):
+	_mapper = None
+	_size = 0
+	_fetching = None
+	_map_surface = None
+	map_position = (0, 0)
+
+	def __init__(self, size, map_position, *args, **kwargs):
+		self._mapper = game.data.Maps()
+		self._size = size
+		self._map_surface = pygame.Surface((size*2, size*2))
+		self.map_position = map_position
+		super(MapSquare, self).__init__((size, size), *args, **kwargs)
+
+	def fetch_map(self):
+		self._fetching = threading.Thread(target=self._internal_fetch_map)
+		self._fetching.start()
+
+	def _internal_fetch_map(self):
+		self._mapper.fetch_grid(self.map_position)
+		self.redraw_map()
+
+	def update(self, *args, **kwargs):
+		super(MapSquare, self).update(*args, **kwargs)
+
+	def render(self, *args, **kwargs):
+		self.fill((0, 0, 0))
+		self.blit(self._map_surface, (0, 0))
+		super(MapSquare, self).render(*args, **kwargs)
+
+	def redraw_map(self, coef=1):
+		self._map_surface.fill((0, 0, 0))
+		for way in self._mapper.transpose_ways((self._size, self._size), (self._size/2, self._size/2)):
+			pygame.draw.lines(
+				self._map_surface,
+				(85, 251, 167),
+				False,
+				way,
+				2
+			)
+		for tag in self._mapper.transpose_tags((self._size, self._size), (self._size/2, self._size/2)):
+			try:
+				basicFont = pygame.font.SysFont(None, 18)
+				text = basicFont.render("%s" % tag[0], True, (95, 255, 177), (0, 0, 0))
+				text_width = text.get_size()[0]
+				pygame.draw.rect(
+				self._map_surface,
+				(0, 0, 0),
+				(tag[1]-5,tag[2]-5,text_width+10,25),
+				0
+			    )
+				self._map_surface.blit(text, (tag[1],tag[2]))
+				pygame.draw.rect(
+				self._map_surface,
+				(95, 255, 177),
+				(tag[1]-5,tag[2]-5,text_width+10,25),
+				1
+			    )
+			except Exception, e:
+				print(e)
+				pass
+
+
+class MapGrid(game.Entity):
+
+	_grid = None
+	_delta = 0.002
+	_starting_position = (0, 0)
+
+	def __init__(self, starting_position, dimensions, *args, **kwargs):
+		self._grid = []
+		self._starting_position = starting_position
+		super(MapGrid, self).__init__(dimensions, *args, **kwargs)
+		self.test_fetch()
+
+	def test_fetch(self):
+		for x in range(10):
+			for y in range(10):
+				square = MapSquare(
+						100,
+						(
+							self._starting_position[0] + (self._delta * x),
+							self._starting_position[1] - (self._delta * y)
+						)
+					)
+				square.fetch_map()
+				square.position = (100 * x, 100 * y)
+				self._grid.append(square)
+
+	def render(self, *args, **kwargs):
+		self.fill((0, 0, 0))
+		for square in self._grid:
+			self.blit(square._map_surface, square.position)
+		super(MapGrid, self).render(*args, **kwargs)
