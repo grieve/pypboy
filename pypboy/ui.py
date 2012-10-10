6fa69fcb7fa22a18fb8c6e788fadf862c5d9b4df
Ryan Grieve <ryan@rehabstudio.com>
2012-10-10 22:52:35 +0100

    made scanlines draw once and move

--- pypboy/ui.py
+++ pypboy/ui.py
@@ -72,27 +72,27 @@ class Footer(game.Entity):
 class Scanlines(game.Entity):
 
 	def __init__(self, width, height, gap, speed, colours):
+		super(Scanlines, self).__init__((width, height))
 		self.width = width
 		self.height = height
 		self.move = gap * len(colours)
 		self.gap = gap
 		self.colours = colours
-		self.offset = 0
+		self.rect[1] = 0
 		self.speed = speed
-		super(Scanlines, self).__init__((width, height))
-
-	def render(self, *args, **kwargs):
 		colour = 0
-		area = pygame.Rect(0, self.offset * self.speed, self.width, self.gap)
+		area = pygame.Rect(0, self.rect[1] * self.speed, self.width, self.gap)
 		while area.top <= self.height - self.gap:
 			self.image.fill(self.colours[colour], area)
 			area.move_ip(0, (self.gap))
 			colour += 1
 			if colour >= len(self.colours):
 				colour = 0
-		self.offset += 1
-		if (self.offset * self.speed) >= self.move:
-			self.offset = 0
+
+	def render(self, *args, **kwargs):
+		self.rect[1] += 1
+		if (self.rect[1] * self.speed) >= self.move:
+			self.rect[1] = 0
 		super(Scanlines, self).render(self, *args, **kwargs)
 
 
