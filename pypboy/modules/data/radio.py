92970d3a53f86eba4dc99fc979c5d8682edd2b7e
Ryan Grieve <me@ryangrieve.com>
2012-10-26 17:57:43 +0100

    genericizing station handling, config now holds radio reference

--- pypboy/modules/data/radio.py
+++ pypboy/modules/data/radio.py
@@ -9,11 +9,24 @@ class Module(pypboy.SubModule):
 
 	def __init__(self, *args, **kwargs):
 		super(Module, self).__init__(*args, **kwargs)
-		self.gnr = entities.GalaxyNewsRadio()
-		self.add(self.gnr)
-		self.gnr.play()
+		self.stations = [
+			entities.GalaxyNewsRadio()
+		]
+		for station in self.stations:
+			self.add(station)
+		self.active_station = None
+		config.radio = self
+
+		self.select_station(0)
+
+	def select_station(self, station):
+		if hasattr(self, 'active_station') and self.active_station:
+			self.active_station.stop()
+		self.active_station = self.stations[station]
+		self.active_station.play_random()
+
 
 	def handle_event(self, event):
-		if event.type ==  config.EVENTS['SONG_END']:
-			if hasattr(self, 'active_station'):
+		if event.type == config.EVENTS['SONG_END']:
+			if hasattr(self, 'active_station') and self.active_station:
 				self.active_station.play_random()
