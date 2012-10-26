7f246880da11e8ce68f4d1bed26110a226316cb0
Ryan Grieve <me@ryangrieve.com>
2012-10-26 14:27:54 +0100

    instantiating radio object

--- pypboy/modules/data/radio.py
+++ pypboy/modules/data/radio.py
@@ -1,6 +1,19 @@
 import pypboy
+import config
 
+from pypboy.modules.data import entities
 
 class Module(pypboy.SubModule):
 
 	label = "Radio"
+
+	def __init__(self, *args, **kwargs):
+		super(Module, self).__init__(*args, **kwargs)
+		self.gnr = entities.GalaxyNewsRadio()
+		self.add(self.gnr)
+		self.gnr.play()
+
+	def handle_event(self, event):
+		if event.type ==  config.EVENTS['SONG_END']:
+			if hasattr(self, 'active_station'):
+				self.active_station.play_random()
